a891db2f1013fd76ea0d0d3f6b4e0cbf3943ad41
KoukiIijima24 <koki.iijima@jbs.com>
2023-05-25 23:05:37 +0900

    add resolver

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 const { createStore } = require("./utils"); //highlight-line
 
 //highlight-start
@@ -11,7 +12,7 @@ const store = createStore(); //highlight-line
 
 const server = new ApolloServer({
   typeDefs,
-  //highlight-start
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store }),
